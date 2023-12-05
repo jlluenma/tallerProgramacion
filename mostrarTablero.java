b990e7f256209845d64ea41da4d0a4afe4b02959
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-12-05 12:08:01 -0500

    Update mostrarTablero.java

--- mostrarTablero.java
+++ mostrarTablero.java
@@ -1,78 +1,38 @@
-package proyectoTallerProgramacion.v4;
+package proyectoTallerProgramacion.v5;
 
 public class mostrarTablero {
-    private char sMina = '◉';
-    private char sVacia = ' ';
-    private char sCeldaCubierta = '▀';
+    public static final int nFilas = 11;
+    public static final int nColumnas = 11;
+    public static final char sCubierto = '▀';
+    private static final String saltoLinea = "\n";
+    public char[][] tablero;
+    public static boolean[][] minas;
+    public static boolean[][] descubiertas;
+    private StringBuilder tableroPrint = new StringBuilder("");
 
-    public int nFilas = 10;
-    private int nColumnas = 10;
-    private int nMinas = 10;
-    private char[][] tablero;
-    private boolean[][] minas;
-    private boolean[][] descubiertas;
-    private boolean[][] cubiertas;
-
-    public mostrarTablero(int nFilas, int nColumnas, int nMinas, char sMina, char sVacia, char sCubierta, boolean[][] descubiertas, boolean[][] cubiertas) {
-        this.nFilas = nFilas;
-        this.nColumnas = nColumnas;
-        this.nMinas = nMinas;
-        this.sMina = sMina;
-        this.sVacia = sVacia;
-        this.sCeldaCubierta = sCubierta;
-        this.descubiertas = descubiertas;
-        this.cubiertas = cubiertas;
-
-        // Crear una instancia de la clase 'tablero'
-        tablero nuevoTablero = new tablero(this);
-
-        // Llamar a la función para inicializar el tablero
-        nuevoTablero.inicializarTablero(sCeldaCubierta);
-
-        // Llamar a la función para colocar las minas
+    public StringBuilder inicializarTablero() {
+        tablero = new char[nFilas][nColumnas];
+        minas = new boolean[nFilas][nColumnas];
+        descubiertas = new boolean[nFilas][nColumnas];
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nColumnas; j++) {
+                tablero[i][j] = sCubierto;
+            }
+            for (i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    tablero[i][j] = sCubierto;
+                }
+            }
+            for (i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    tableroPrint.append("| ").append(tablero[i][j]).append(" ");
+                }
+                tableroPrint.append("|").append(saltoLinea);
+            }
+        }
+        return tableroPrint;
     }
-
-    public int getnFilas() {
-        return nFilas;
-    }
-
-    public void setnFilas(int nFilas) {
-        this.nFilas = nFilas;
-    }
-
-    public int getnColumnas() {
-        return nColumnas;
-    }
-
-    public int getnMinas() {
-        return nMinas;
-    }
-
-    public char getsMina() {
-        return sMina;
-    }
-
-    public void setMinas(boolean[][] minas) {
-        this.minas = minas;
-    }
-
-    public char getsVacia() {
-        return sVacia;
-    }
-
-    public char getsCeldaCubierta() {
-        return sCeldaCubierta;
-    }
-
-    public boolean[][] getMinas() {
-        return minas;
-    }
-
-    public boolean[][] getDescubiertas() {
-        return descubiertas;
-    }
-
-    public boolean[][] getCubiertas() {
-        return cubiertas;
+    public char [][] getTablero(){
+        return tablero;
     }
 }
