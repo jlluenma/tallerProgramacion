3e775a4d7807e95255380d65e2fe94e18553403d
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-12-05 23:22:31 -0500

    Update juego.java

--- juego.java
+++ juego.java
@@ -10,30 +10,31 @@ public class juego {
     boolean[][] descubiertas = mostrarTablero.descubiertas;
     private static final char sMina = '◉';
 
-    public void descubrirCelda(int nFilas, int nColumnas) {
-        if (nuevoValidador.coordenadasValidas(nFilas, nColumnas) && !descubiertas[nFilas][nColumnas]) {
-            descubiertas[nFilas][nColumnas] = true;
-            if (minas[nFilas][nColumnas]) {
-                tablero[nFilas][nColumnas] = sMina;
+    public char [][] descubrirCelda(int fila, int columna) {
+        if (nuevoValidador.coordenadasValidas(fila, columna) && !descubiertas[fila][columna]) {
+            descubiertas[fila][columna] = true;
+            if (minas[fila][columna]) {
+                tablero[fila][columna] = sMina;
                 mismoTablero.inicializarTablero();
                 System.out.println("¡Has perdido! Has encontrado una mina.");
                 System.exit(0);
             } else {
-                int minasCercanas = nuevoContador.contarMinasCercanas(nFilas, nColumnas);
+                int minasCercanas = nuevoContador.contarMinasCercanas(fila, columna);
                 if (minasCercanas > 0) {
-                    tablero[nFilas][nColumnas] = (char) (minasCercanas + '0');
+                    tablero[fila][columna] = (char) (minasCercanas + '0');
                 } else {
-                    tablero[nFilas][nColumnas] = sCubierto; //sVacias
-                    descubrirCelda(nFilas - 1, nColumnas - 1);
-                    descubrirCelda(nFilas - 1, nColumnas);
-                    descubrirCelda(nFilas - 1, nColumnas + 1);
-                    descubrirCelda(nFilas, nColumnas - 1);
-                    descubrirCelda(nFilas, nColumnas + 1);
-                    descubrirCelda(nFilas + 1, nColumnas - 1);
-                    descubrirCelda(nFilas + 1, nColumnas);
-                    descubrirCelda(nFilas + 1, nColumnas + 1);
+                    tablero[fila][columna] = sCubierto; //sVacias
+                    descubrirCelda(fila - 1, columna - 1);
+                    descubrirCelda(fila - 1, columna);
+                    descubrirCelda(fila - 1, columna + 1);
+                    descubrirCelda(fila, columna - 1);
+                    descubrirCelda(fila, columna + 1);
+                    descubrirCelda(fila + 1, columna - 1);
+                    descubrirCelda(fila + 1, columna);
+                    descubrirCelda(fila + 1, columna + 1);
                 }
             }
         }
+        return tablero;
     }
 }
