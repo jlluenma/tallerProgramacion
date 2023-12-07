103c473329d745e69ba51f65d3eb4b15e9c8155e
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-12-07 18:52:06 -0500

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,21 +1,8 @@
-package proyectoTallerProgramacion.v5;
-
-import java.util.Scanner;
+package proyectoTallerProgramacion.v6;
 
 public class principal {
     public static void main(String[] args) {
-        mostrarTablero tablero = new mostrarTablero();
-        StringBuilder tableroInicial = tablero.inicializarTablero();
-        lectorDatos establecerCoordenadas = new lectorDatos();
-        juego nuevoJuego = new juego();
-        Scanner scann = new Scanner(System.in);
-
-            System.out.println(tableroInicial);
-            System.out.println("Ingrese la fila: ");
-            int fila = scann.nextInt();
-            System.out.println("Ingrese la columna: ");
-            int columna = scann.nextInt();
-            //char[][] tableroA = nuevoJuego.descubrirCelda(fila, columna);
-            establecerCoordenadas.establecerN(fila, columna);
+        jugar nuevoJuego = new jugar();
+        nuevoJuego.jugada();
     }
 }
