473e3ecbffb4f0b3ac516d16ecec08f45c65c9fa
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-12-07 20:59:24 -0500

    Update jugar.java

--- jugar.java
+++ jugar.java
@@ -1,5 +1,6 @@
 package proyectoTallerProgramacion.v6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class jugar {
@@ -28,12 +29,41 @@ public class jugar {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             nuevoTablero.mostrarTablero(cTabla,bDescubiertas);
-            System.out.print("Ingresa la fila (0-" + (nFilas - 1) + "): ");
-            int fila = scanner.nextInt();
-            System.out.print("Ingresa la columna (0-" + (nColumnas - 1) + "): ");
-            int columna = scanner.nextInt();
-            descubrirCeldas(fila, columna);
+            try {
+                System.out.print("Ingresa la fila (0-" + (nFilas - 1) + "): ");
+                int fila = scanner.nextInt();
+                System.out.print("Ingresa la columna (0-" + (nColumnas - 1) + "): ");
+                int columna = scanner.nextInt();
+                if ((fila >= 0 && fila < nFilas) && (columna>=0 && columna < nColumnas )){
+                    descubrirCeldas(fila, columna);
+                }else {
+                    System.out.println("Coordenadas fuera de los límites del tablero. Inténtalo nuevamente");
+                }
+                // Verificar condición de victoria
+                if (todasCeldasDescubiertas()) {
+                    System.out.println("""
+                             *:・ﾟ✧*:・ﾟ✧*:・ﾟ✧*:・ﾟ✧(｡◕‿‿◕｡)*:・ﾟ✧*:・ﾟ✧*:・ﾟ✧*:・ﾟ✧
+                            ¡Felicidades! Has descubierto todas las celdas sin minas. ¡Has ganado!
+                            """);
+                    System.exit(0);
+                }
+            }catch (InputMismatchException e) {
+                System.out.println("Error: Debes ingresar números enteros. Intenta nuevamente.");
+                scanner.nextLine();  // Limpiar el buffer del scanner
+            }
         }
+    }
+
+    // Método para verificar si todas las celdas sin minas han sido descubiertas
+    private boolean todasCeldasDescubiertas() {
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nColumnas; j++) {
+                if (!bMinas[i][j] && !bDescubiertas[i][j]) {
+                    return false; // Hay al menos una celda sin mina no descubierta
+                }
+            }
+        }
+        return true; // Todas las celdas sin minas han sido descubiertas
     }
 
     public void descubrirCeldas(int nfila, int ncolumna){
@@ -42,7 +72,7 @@ public class jugar {
             if (bMinas[nfila][ncolumna]) {
                 cTabla[nfila][ncolumna] = cMina;
                 nuevoTablero.mostrarTablero(cTabla,bDescubiertas);
-                System.out.println("¡Has perdido! Has encontrado una mina.");
+                System.out.println("¡Has perdido! Has encontrado una mina.ಡ_ಡ");
                 System.exit(0);
             } else {
                 //verificar si la cantidad de  jugadas, si es menor al límite el jugador ganó
