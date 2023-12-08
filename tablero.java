ea49efb3fda4c3af172423525b25af4c6ea46fe0
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-12-07 19:04:31 -0500

    Update and rename mostrarTablero.java to tablero.java

---
+++ tablero.java
@@ -0,0 +1,47 @@
+package proyectoTallerProgramacion.v6;
+
+public class tablero {
+    private static int nFilas = 11;
+    private static int nColumnas = 11;
+    private static  char sCubierto = '▀';
+    private static char celdaVacia = ' ';
+
+    public static void inicializarTablero(char [][] cTabla) {
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nColumnas; j++) {
+                cTabla[i][j] = sCubierto;
+            }
+        }
+    }
+
+    public void mostrarTablero(char [][] cTabla, boolean[][] bDescubiertas) {
+        System.out.print("TABLERO DE BUSCAMINAS(9x9) \n");
+        System.out.print("\n  ");
+        for (int j = 0; j < nColumnas; j++) { //Imprime el encabezado con el número de columnas
+            System.out.print(" " + j + "  ");
+        }
+        System.out.print("\n  ");
+        for (int j = 0; j < nColumnas; j++) {
+            System.out.print("----");
+        }
+        System.out.println();
+        for (int i = 0; i < nFilas; i++) {
+            System.out.print(i + "|");
+            for (int j = 0; j < nColumnas; j++) {
+                if (bDescubiertas[i][j]) {
+                    System.out.print(" " + cTabla[i][j] + " |");
+                } else {
+                    System.out.print(" " + sCubierto + " |");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static int getnFilas() {
+        return nFilas;
+    }
+    public static int getnColumnas() {
+        return nColumnas;
+    }
+    public static char getCeldaVacia() {return celdaVacia;}
+}
